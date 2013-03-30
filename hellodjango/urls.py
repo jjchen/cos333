ed9d17f9fe0893644a217f386a613b0be336b329
stanley <stanley@stanley-VirtualBox.(none)>
2013-03-30 00:32:29 -0400

    fixed urls.py

--- hellodjango/urls.py
+++ hellodjango/urls.py
@@ -4,6 +4,10 @@ from django.conf.urls import patterns, include, url
 # from django.contrib import admin
 # admin.autodiscover()
 
+def current_datetime():
+    print "test 1"
+    return 2019
+
 urlpatterns = patterns('',
                        (r'^time/$', current_datetime)
     # Examples:
