d6df74942503993974cbf1c9695b4978fa9d0646
stanley <stanley@stanley-VirtualBox.(none)>
2013-03-30 04:50:35 -0400

    added settings.py

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -9,15 +9,27 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/home/stanley/cos333-oc/db',
+        'NAME': 'db',
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'HOST': '',                    
+        'PORT': '',                    
+    }
+}'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd3mruqbl01mt9e',
+        # The following settings are not used with sqlite3:
+        'USER': 'gvvmlwjnckcrtz',
+        'PASSWORD': '8S9bKm59d8esQj7BYH1PbE9VP5',
+        'HOST': 'ec2-54-225-69-193.compute-1.amazonaws.com',                    
+        'PORT': '54',                    
     }
 }
 
