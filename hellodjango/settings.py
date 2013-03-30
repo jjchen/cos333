ce909e0dee326a08d376d7dd878409d9be3c07aa
stanley <stanley@stanley-VirtualBox.(none)>
2013-03-30 00:11:22 -0400

    Added database credentials to hellodjango/settings.py

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -11,13 +11,13 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.', 'postgresql_psycopg2'# Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'd3mruqbl01mt9e',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'USER': 'gvvmlwjnckcrtz',
+        'PASSWORD': '8S9bKm59d8esQj7BYH1PbE9VP5',
+        'HOST': 'ec2-54-225-69-193.compute-1.amazonaws.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '54',                      # Set to empty string for default.
     }
 }
 
