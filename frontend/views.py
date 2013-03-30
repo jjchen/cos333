c47fff78c5e7a743a5af06d70d9bca2de4282fa7
jjchen <junjunc@princeton.edu>
2013-03-30 14:42:38 -0400

    I just don't know python

--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 # Create your views here.
 def index(request):
-    context = {'events_list': events_list}
+	events_list = []
+	context = {'events_list': events_list}
     return render(request, 'frontend/map.html', context)
