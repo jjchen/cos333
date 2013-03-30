23e6bef5a7972a7c1ec694db384e9acf904b02e7
stanley <stanley@stanley-VirtualBox.(none)>
2013-03-30 16:48:15 -0400

    added models to frontend, to provide database tables for add event form

--- frontend/models.py
+++ frontend/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+NAME_MAXLEN=50
+class User(models.Model):
+    name = models.CharField(max_length=NAME_MAXLEN)
+    netid = models.CharField(max_length=20)
+    friends = models.ForeignKey('self') #recursive relation
+
+class Group(models.Model):
+    users = models.ManyToManyField(User)
+    name = models.CharField(max_length=NAME_MAXLEN)
+    
+class Event(models.Model):
+    name = models.CharField(max_length=NAME_MAXLEN)
+    creator = models.ForeignKey(User)
+    startTime = models.DateTimeField()
+    endTime = models.DateTimeField()
+    locName = models.CharField(max_length=NAME_MAXLEN)
+    locLat = models.FloatField()
+    locLong = models.FloatField()
