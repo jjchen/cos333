79fd7a3ce987d83334e810cb925f9afaf8ad2fd0
stanley <stanley@stanley-VirtualBox.(none)>
2013-03-30 02:22:53 -0400

    installed apps

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -11,12 +11,13 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', 'postgresql_psycopg2'# Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'd3mruqbl01mt9e',                      # Or path to database file if using sqlite3.
+#        'ENGINE':'test',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd3mruqbl01mt9e',                      
         # The following settings are not used with sqlite3:
         'USER': 'gvvmlwjnckcrtz',
         'PASSWORD': '8S9bKm59d8esQj7BYH1PbE9VP5',
-        'HOST': 'ec2-54-225-69-193.compute-1.amazonaws.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'HOST': 'ec2-54-225-69-193.compute-1.amazonaws.com',                    
         'PORT': '54',                      # Set to empty string for default.
     }
 }
@@ -120,6 +121,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls'
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
