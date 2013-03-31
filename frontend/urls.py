62124458a5b5819400256ef86407603388cd4dc1
jjchen <junjunc@princeton.edu>
2013-03-30 22:59:43 -0400

    forms

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,4 +3,5 @@ from frontend import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index')
+    url(r'^/add/$', views.add, name='add')
 )
--- frontend/views.py
+++ frontend/views.py
@@ -6,3 +6,17 @@ def index(request):
 	events_list = NewEvent.objects.all()
 	context = {'events_list': events_list}
 	return render(request, 'frontend/map.html', context)
+
+def add(request):
+	# get fields
+	new_name = request.POST['event_name']
+	new_date = request.POST['date']
+	new_time = request.POST['start_time']
+	new_location = request.POST['location']
+	
+	# add to database
+	new_event = NewEvent(name=new_name, date=new_date, time=new_time, location=new_location)
+	new_event.save()
+
+	# return to index
+	return HttpResponseRedirect(reverse('polls:index'))
