66887476fe2c2f32a37ca571bb43581b33e3b151
jjchen <junjunc@princeton.edu>
2013-03-30 21:34:35 -0400

    ...

--- frontend/models.py
+++ frontend/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.utils import timezone
+import datetime
 # Create your models here.
+class NewEvent(models.Model):
+    name = models.CharField(max_length=200)
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length=200)
+    def __unicode__(self):
+        return self.name
--- frontend/views.py
+++ frontend/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from frontend.models import NewEvent
+
 # Create your views here.
 def index(request):
-	events_list = []
+	events_list = NewEvent.objects.all()
 	context = {'events_list': events_list}
 	return render(request, 'frontend/map.html', context)
