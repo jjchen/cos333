e767382e65acbd3de0a80cd3c3bff6ac8a217a36
Alexander Zhao <lz@princeton.edu>
2013-04-12 17:30:05 -0400

    south addition

--- oc/settings.py
+++ oc/settings.py
@@ -143,6 +143,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'social_auth'
+    'south'
 )
 
 # A sample logging configuration. The only tangible logging
