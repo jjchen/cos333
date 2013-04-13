04edfe53d44dc8ac8d5b5668713627ae1c431be9
Stanley Winford <mzzhang@princeton.edu>
2013-04-13 17:00:14 -0400

    seemed that South is working

--- frontend/models.py
+++ frontend/models.py
@@ -48,8 +48,6 @@ class MyGroup(models.Model):
                                  
     
 class Event(models.Model):
-   # testField = models.CharField(max_length=40, default="ihavenoideawhatiamdoing")
-#    testField2 = models.CharField(max_length=40, default="ihavenoideawhatamdoing")
     name = models.CharField(max_length=NAME_MAXLEN)
     creator = models.ForeignKey(MyUser)
     startTime = models.DateTimeField()
