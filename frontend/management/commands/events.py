7185787c17d2f4d31bf35720a370f6aa0a0ebb80
Alexander Zhao <lz@princeton.edu>
2013-04-16 17:46:23 -0400

    consolidated various feeds

--- frontend/management/commands/events.py
+++ frontend/management/commands/events.py
@@ -7,43 +7,47 @@ import json
 import urllib2
 
 class Command(BaseCommand):
+    
+    def handle(self, *args, **options): 
+        feeds = ["http://etcweb.princeton.edu/webfeedsx/events/?fmt=json",
+                 "http://etcweb.princeton.edu/webfeeds/events/roxen/?fmt=json",
+                 "http://etcweb.princeton.edu/webfeeds/events/usg/?fmt=json"]
 
-	def handle(self, *args, **options): 
+        for url in feeds:
+            resp = urllib2.urlopen(url)
+            json_obj = json.load(resp)
+            
+            events = json_obj['events']
+            
+            for event in events:
+                try:
+                    NewEvent.objects.get(name = event['title'])
 
-		feeds = ["http://etcweb.princeton.edu/MobileFeed/events/?fmt=json", "http://etcweb.princeton.edu/webfeeds/events/roxen/?fmt=json", "http://etcweb.princeton.edu/webfeeds/events/usg/?fmt=json"]
-		for url in feeds:
-			resp = urllib2.urlopen(url)
-			json_obj = json.load(resp)
-
-			events = json_obj['events']
-
-			for event in events:
-				try:
-					NewEvent.objects.get(name = event['title'])
-				except NewEvent.DoesNotExist:
-					print event
-					name = event['title']
-					building = event['locationName']
-					latitude = event['latitude']
-					longitude = event['longitude']
-					startDate = event['startDate']
-					startTime = event['startTime']
-					#endDate = event['endDate']
-					#endTime = event['endTime']
-					#description = event['description']
-					#audience = event['audience']
-					#tags = event['categories']['categoryName']
-
-					# format date
-					date = datetime.datetime.strptime(startDate['0'], "%Y-%m-%d").date()
-
-					parts = startTime.split()
-
-					# format time
-					time = datetime.datetime.strptime(parts[0], "%H:%M:%S").time()
-
-					# make into tags
-					#categories = event['categories']
-
-					new_event = NewEvent(name=name, date=date, time=time, location=building, lat=latitude, lon=longitude, tags="")
-					new_event.save()
+                except NewEvent.DoesNotExist:
+                    print event
+                    name = event['title']
+                    building = event['locationName']
+                    latitude = event['latitude']
+                    longitude = event['longitude']
+                    startDate = event['startDate']
+                    startTime = event['startTime']
+                    #endDate = event['endDate']
+                    #endTime = event['endTime']
+                    #description = event['description']
+                    #audience = event['audience']
+                    #tags = event['categories']['categoryName']
+                    
+                    # format date
+                    date = datetime.datetime.strptime(startDate['0'], "%Y-%m-%d").date()
+                    
+                    parts = startTime.split()
+                    
+                    # format time
+                    time = datetime.datetime.strptime(parts[0], "%H:%M:%S").time()
+                    
+                    # make into tags
+                    #categories = event['categories']
+                    
+                    new_event = NewEvent(name=name, date=date, time=time, location=building, lat=latitude, lon=longitude, tags="")
+                    new_event.save()
+                    
--- frontend/urls.py
+++ frontend/urls.py
@@ -2,12 +2,14 @@ from django.conf.urls import patterns, include, url
 from frontend import views
 
 urlpatterns = patterns('',
-    url(r'^settings/', views.settings, name='settings'),
-    url(r'^signup/', views.signup),
     url(r'^$', views.index, name='index'),
+
+    url(r'^signup/', views.signup),
+    url(r'^settings/', views.settings, name='settings'),
+    url(r'', include('social_auth.urls')),
+
     url(r'^add/$', views.add, name='add'),
     url(r'^addgroup/', views.addgroup, name='addgroup'),
     url(r'^(?P<group>\w+)/$', views.rmgroup, name='rmgroup'),
-    url(r'^search/$', views.search, name='search'),
-    url(r'', include('social_auth.urls'))
+    url(r'^search/$', views.search, name='search')
 )
