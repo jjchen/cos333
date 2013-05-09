1b8858bde57a1b3685285f7e063090c491d4ca87
Alexander Zhao <lz@princeton.edu>
2013-05-08 20:48:07 -0400

    personal bug

--- frontend/facebook.py
+++ frontend/facebook.py
@@ -7,6 +7,18 @@ from django.http import HttpResponseRedirect
 from django.core.exceptions import ObjectDoesNotExist
 from frontend.models import NewEvent
 
+from django.contrib.auth import BACKEND_SESSION_KEY
+from django.contrib.auth.models import AnonymousUser
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
+
+from django.views.decorators.csrf import csrf_exempt
+from django.core.cache import cache
+
+from social_auth.views import complete as social_complete
+from social_auth.utils import setting
+from social_auth.backends.facebook import load_signed_request, FacebookBackend
+
 # Facebook decorator to setup environment
 def facebook_decorator(func):
     def wrapper(request, *args, **kwargs):
