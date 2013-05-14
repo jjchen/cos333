96bbaddd88fe8c896b2a0981bd0f8c73a6c7bb5e
Alexander Zhao <lz@princeton.edu>
2013-05-14 02:12:43 -0400

    changed login error message

--- oc/settings.py
+++ oc/settings.py
@@ -4,12 +4,12 @@ DEBUG = False
 TEMPLATE_DEBUG = False
 
 # Public: www.cos333-oc.herokuapp.com/
-#FACEBOOK_APP_ID = '431733443585073'
-#FACEBOOK_API_SECRET = 'bee41bd237e61feb159d64f99e7db996'
+FACEBOOK_APP_ID = '431733443585073'
+FACEBOOK_API_SECRET = 'bee41bd237e61feb159d64f99e7db996'
 
 # Private: localhost:8000
-FACEBOOK_APP_ID =  '125667410957888'
-FACEBOOK_API_SECRET = '1d71ff77879df12aa55ba2307523ab04'
+#FACEBOOK_APP_ID =  '125667410957888'
+#FACEBOOK_API_SECRET = '1d71ff77879df12aa55ba2307523ab04'
 
 FACEBOOK_EXTENDED_PERMISSIONS = ['user_events', 'friends_events',
                                  'create_event', 'rsvp_event', 'user_groups',
