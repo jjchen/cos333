2f30b7bde4b2731353099125a40b53fd7bc47961
Alexander Zhao <lz@princeton.edu>
2013-05-14 09:27:21 -0400

    collectstatic

--- oc/settings.py
+++ oc/settings.py
@@ -162,7 +162,7 @@ TEMPLATE_DIRS = (
 )
 
 INSTALLED_APPS = (
-	'django.contrib.auth',
+    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
